7cdd19a65b553cd8547df0c907b4599b07353737
alejant <alejandro.hunt@bue.edu.ar>
2024-06-28 17:53:40 -0300

    Actualizado proyecto para ser integrado con flask api

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-**Proyecto Integrador Full Stack de Ejemplo de la Comisión 24266**
+# cac-video
+Es el respositorio en el que podemos ir trabajando para generar la versión final del proyecto integral de fullstack codo a codo
+
+[Repositorio del proyecto publico](https://github.com/broko-de/cac-movies)
+[Visualización del proyecto](https://broko-de.github.io/cac-movies/)
+
+
--- static/js/fetch-movies.js
+++ static/js/fetch-movies.js
@@ -8,57 +8,29 @@ const options = {
   }
 };
 
-const renderTopRated = (miVariable) => {
+const renderTopRated = (movie) => {
   let html = `
       <div class="movie-item-v2">
         <div class="wrapper">
-            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${miVariable.poster_path}" alt="" class="movie-item-img-v2">
+            <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}" alt="" class="movie-item-img-v2">
         </div>
         <div class="movie-item-detail-v2">
-            <p class="movie-item-detail-title-v2">${miVariable.title}</p>
-            <p class="movie-item-detail-subtitle-v2">${miVariable.release_date} / ${miVariable.vote_average} ⭐</p>
+            <p class="movie-item-detail-title-v2">${movie.title}</p>
+            <p class="movie-item-detail-subtitle-v2">${movie.release_date} / ${movie.vote_average} ⭐</p>
         </div>
       </div>
   `
   return html;
 }
 
-// function renderTopRated(movie) {
-//   let html = `
-//       <div class="movie-item-v2">
-//         <div class="wrapper">
-//             <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}" alt="" class="movie-item-img-v2">
-//         </div>
-//         <div class="movie-item-detail-v2">
-//             <p class="movie-item-detail-title-v2">${movie.title}</p>
-//             <p class="movie-item-detail-subtitle-v2">${movie.release_date} / ${movie.vote_average} ⭐</p>
-//         </div>
-//       </div>
-//   `
-//   return html;
-// }
-
-
 fetch(`${URLSERVER}/movie/top_rated`, options)
   .then(response => response.json())
-  .then(json_response => {
-          console.log(json_response)
-          let miArray = json_response.results;
+  .then(response => {
+          console.log(response)
+          let movies = response.results;
           let divTopRated = document.querySelector('#list-top-rated');
-          //let divTopRated = document.getElementById('list-top-rated');
-          miArray.forEach(elemento => {
-         //   let html = renderTopRated(elemento);
-            let  html = `
-            <div class="movie-item-v2">
-              <div class="wrapper">
-                  <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/${elemento.poster_path}" alt="" class="movie-item-img-v2">
-              </div>
-              <div class="movie-item-detail-v2">
-                  <p class="movie-item-detail-title-v2">${elemento.title}</p>
-                  <p class="movie-item-detail-subtitle-v2">${elemento.release_date} / ${elemento.vote_average} ⭐</p>
-              </div>
-            </div>
-          `
+          movies.forEach(movie => {
+            let html = renderTopRated(movie);
             divTopRated.insertAdjacentHTML('beforeend',html)
           });
         }
